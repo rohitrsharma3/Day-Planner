0d4556e3594164e3f499b74cae63bc20e737feef
Joshua Utting <49527676+utting98@users.noreply.github.com>
2019-12-22 13:15:01 +0000

    Fixed to self refresh at specified interval

--- backend.py
+++ backend.py
@@ -3,6 +3,7 @@ import csv
 import datetime
 from datetime import datetime as dt
 import os
+from tkinter import *
 flag = True
 
 def writeToLog(now):
@@ -10,10 +11,11 @@ def writeToLog(now):
         log.write('email sent at '+ str(now))
 
 def checkTime():
+    global flag, backendroot
     a = None
     with open('C:\\Users\\rohit\\Desktop\\Code\\Day-Planner\\remindersData.csv','r') as file:
-        csv_reader = csv.reader(file)
-        next(csv_reader)
+        #csv_reader = csv.reader(file) #these lines cause errors but are not necessary
+        #next(csv_reader)
         for line in file:
             listLine = line.split(',')
             startTime = listLine[-2]
@@ -40,7 +42,8 @@ def checkTime():
                         print('ERROR')
                         flag = False
                         break
-                    
+
+    backendroot.after(1000,checkTime) #repeat checking process after x milliseconds for continuous background checking                
  
 def mail(From, to, body):
     gmail_usr = ""
@@ -53,6 +56,10 @@ def mail(From, to, body):
     server.close()
     print("EMAIL SENT!!!!")
 
-while flag == True:
-    checkTime()
+#while flag == True:
+if __name__ == '__main__': #run this code only if this script is ran directly not when imported
+    backendroot = Tk() #define a tkinter window for using the after method
+    backendroot.withdraw() #make this window invisible as it is blank
+    checkTime() #run checktime function
+    backendroot.mainloop() #make window 'display' but is invisible
 
